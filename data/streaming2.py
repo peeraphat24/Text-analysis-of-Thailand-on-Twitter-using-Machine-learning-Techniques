dfe173501081e687fbb2a8fc9e1499bc60c98a65
Peeraphat <peeraphat-tawan@hotmail.com>
2019-03-08 16:36:46 +0700

    Project

--- data/streaming2.py
+++ data/streaming2.py
@@ -3,10 +3,10 @@ import csv
 import datetime
 import codecs
 
-api_key = 'I5gf0T8YkfHIRUutDErRDLtFi'
-api_secret = 'DeM1Rp6KsrOYPALcInD5WPTWS7UmI6CPUPdDJXsGgNbjE6pE2A'
-access_token = '892969681212002308-0clEM1ooK8MnHcNzf9gcS6QhO5YC2eR'
-access_secret = 'aKBr9SCQz7qbK4HfQdJHyymKwgdkUwcF6cg4Kq8MRF31e'
+api_key = ''
+api_secret = ''
+access_token = ''
+access_secret = ''
 TRACK_TERM = 'thailand'
 
 api = TwitterAPI(api_key,
